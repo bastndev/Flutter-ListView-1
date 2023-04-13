130b9ee43e4f24f07e7cd6e7e2fa78b10c7aa1fb
bastndev <gohit24@gmail.com>
2023-04-13 14:52:48 -0500

    rename

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -1,15 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:github_flutter_1/theme/app_team.dart';
 
 class CardScreen extends StatelessWidget {
-   
+  
   const CardScreen({Key? key}) : super(key: key);
   
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('CardScreen'),
+    return  Scaffold(
+      appBar: AppBar(
+        title:const  Text('Card Widget'),
       ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 50,vertical: 10),
+        children:  [
+
+        Card(
+          child: Column(
+            children: const [
+              ListTile(
+                leading: Icon(
+                Icons.photo,color: AppTheme.primary),
+                title: Text('Im Title'),
+                subtitle: Text('Anim ex culpa consectetur enim consequat enim excepteur elit esse labore.'),
+              )
+            ],
+          ),
+        )
+
+
+        ],
+      )
     );
   }
 }
