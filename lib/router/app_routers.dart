1badf19f86882eb9560eff38d6e23aa2f59fe8ef
bastndev <gohit24@gmail.com>
2023-04-16 19:59:59 -0500

    Avatar

--- lib/router/app_routers.dart
+++ lib/router/app_routers.dart
@@ -8,11 +8,36 @@ class AppRoutes {
   static const initialRoute = 'home';
 
   static final menuOptions = <MenuOptions>[
-    MenuOptions( route: 'home',name: 'Home Screen',screen: const HomeScreen(),icon: Icons.home),
-    MenuOptions( route: 'ListView1',name: 'ListView tipe 1',screen: const ListView1Screen(),icon: Icons.filter_1),
-    MenuOptions( route: 'ListView2',name: 'ListView tipe 1',screen: const ListView2Screen(),icon: Icons.filter_2),
-    MenuOptions( route: 'Alert',name: 'Alert Screen',screen: const AlertScreen(),icon: Icons.add_alert),
-    MenuOptions( route: 'Card',name: 'Tarjetas - Card',screen: const CardScreen(),icon: Icons.credit_card),
+    MenuOptions(
+        route: 'home',
+        name: 'Home Screen',
+        screen: const HomeScreen(),
+        icon: Icons.home),
+    MenuOptions(
+        route: 'ListView1',
+        name: 'ListView tipe 1',
+        screen: const ListView1Screen(),
+        icon: Icons.filter_1),
+    MenuOptions(
+        route: 'ListView2',
+        name: 'ListView tipe 1',
+        screen: const ListView2Screen(),
+        icon: Icons.filter_2),
+    MenuOptions(
+        route: 'Alert',
+        name: 'Alert Screen',
+        screen: const AlertScreen(),
+        icon: Icons.add_alert),
+    MenuOptions(
+        route: 'Card',
+        name: 'Tarjetas - Card',
+        screen: const CardScreen(),
+        icon: Icons.credit_card),
+    MenuOptions(
+        route: 'avatar',
+        name: 'Circle Avatar',
+        screen: const AvatarScreen(),
+        icon: Icons.supervised_user_circle_outlined),
   ];
 
   static Map<String, Widget Function(BuildContext)> getAppRoutes() {
