f1ba8e2ad9eff33244acc87fee6406265530373a
bastndev <gohit24@gmail.com>
2023-04-19 11:22:24 -0500

    :recycle: Vercion 4.0.1 (5)

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -14,17 +14,43 @@ class InputssScreen extends StatelessWidget {
             padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
             child: Column(children: [
               TextFormField(
-                autofocus: true,
-                initialValue: 'Gohit Mayer',
+                autofocus: false,
+                initialValue: '',
                 textCapitalization: TextCapitalization.words,
                 onChanged: (value) {
                   print('value: $value');
                 },
+                validator: (value) {
+                  if (value == null) return 'Este Campo es requerido';
+                  return value.length < 3 ? 'Minimo de 3 letras ' : null;
+                },
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                decoration: const InputDecoration(
+                  hintText: 'Name User',
+                  labelText: 'Name',
+                  helperText: 'Only lyrics',
+                  counterText:'3 caracter',
+                  // prefixIcon: Icon(Icons.group_outlined, color: Colors.red,),
+                  suffixIcon: Icon(Icons.group_outlined, color: Colors.red,),
+                  icon:Icon(Icons.group_outlined, color: Colors.red,),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.black
+                    )
+                  ),
+                  /* --- --- --- --- --- Borde box */
+                    border:OutlineInputBorder(
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(10),
+                        // bottomRight: Radius.circular(10),
+                        topRight: Radius.circular(10),
+                        // topLeft: Radius.circular(10),
+                      )
+                    )
+                ),
               )
-            ]
+            ]),
           ),
-        ),
-      )
-    );
+        ));
   }
 }
